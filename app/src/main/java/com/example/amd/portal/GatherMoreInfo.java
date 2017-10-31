68bbbb62ba4fca86c6664623d7aa7d11e6880edf
AMD <jyorkcar@sbcglobal.net>
2017-10-31 12:39:06 -0700

    Upgrade3 File io added.

--- app/src/main/java/com/example/amd/portal/GatherMoreInfo.java
+++ app/src/main/java/com/example/amd/portal/GatherMoreInfo.java
@@ -1,11 +1,13 @@
 package com.example.amd.portal;
 
+import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.*;
 import android.view.View;
@@ -20,11 +22,16 @@ import android.widget.EditText;
 import android.text.format.DateFormat;
 import android.widget.TimePicker;
 
-import java.io.BufferedWriter;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Calendar;
-import java.util.Locale;
+
 
 /**
  * Created by buscis-c2-l3 on 9/18/2017.
@@ -34,13 +41,18 @@ public class GatherMoreInfo extends AppCompatActivity implements AdapterView.OnI
 {
     private ArrayAdapter<CharSequence> adapter;
 
-    //private Spinner sp;
+    private static final String FILE_NAME="config.txt";
 
     private TextView    user;
 
     private EditText    name,
                         address,
                         email;
+
+    private String  language,
+                    memberType;
+
+    private Spinner spinner;
 
     private static EditText date,
                             time;
@@ -51,11 +63,6 @@ public class GatherMoreInfo extends AppCompatActivity implements AdapterView.OnI
     private Button b;
 
     private Intent intent;
-
-    private static final String ENGLISH = Locale.ENGLISH.toString(); //English language locale.
-    private static final String SPANISH = ""; //Spanish language locale.
-
-    private String selectedLanguage = "";
 
     private Intent i;
 
@@ -68,7 +75,6 @@ public class GatherMoreInfo extends AppCompatActivity implements AdapterView.OnI
         date     = (EditText) findViewById(R.id.editTextDate);
         time     = (EditText) findViewById(R.id.editTextTime);
 
-        //sp       = (Spinner)findViewById(R.id.spinner);
         rbEng    = (RadioButton)findViewById(R.id.rbEng);
         rbSpan   = (RadioButton)findViewById(R.id.rbSpan);
         user     = (TextView)findViewById(R.id.user);
@@ -76,44 +82,35 @@ public class GatherMoreInfo extends AppCompatActivity implements AdapterView.OnI
         address  = (EditText)findViewById(R.id.editTextAddress);
         email    = (EditText)findViewById(R.id.editTextEmail);
 
+        spinner = (Spinner)findViewById(R.id.spinner);
+
         b = (Button)findViewById(R.id.button4);
 
-        adapter = ArrayAdapter.createFromResource(this,R.array.dates,R.layout.support_simple_spinner_dropdown_item);
+        //adapter = ArrayAdapter.createFromResource(this,R.array.memberships,R.layout.support_simple_spinner_dropdown_item);
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        //sp.setAdapter(adapter);
+        //spinner.setAdapter(adapter);
 
-        //sp.setOnItemSelectedListener(this);
+        spinner.setOnItemSelectedListener(this);
 
         i = getIntent();
         user.setText(i.getStringExtra(MainActivity.PACKAGE_NAME));
+
+        openFile("", this);
 
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long id)
     {
-        date.setText(parent.getItemAtPosition(pos).toString());
+        //date.setText(parent.getItemAtPosition(pos).toString());
+        memberType = spinner.getSelectedItem().toString();
     }
 
     public void onNothingSelected(AdapterView<?> parent)
     {
         //Do nothing.
-    }
-
-
-    private void changeLanguage(String lang)
-    {
-        Locale myLocale = new Locale(lang);
-        Resources res = getResources();
-        DisplayMetrics dm = res.getDisplayMetrics();
-        Configuration conf = res.getConfiguration();
-//        conf.locale = myLocale;
- //       res.updateConfiguration(conf, dm);
-        onConfigurationChanged(conf);
-    /*Intent refresh = new Intent(this, AndroidLocalize.class);
-    startActivity(refresh);*/
     }
 
     @Override
@@ -127,29 +124,7 @@ public class GatherMoreInfo extends AppCompatActivity implements AdapterView.OnI
     @Override
     public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
 
-
         radioGroup.getCheckedRadioButtonId();
-
-        if(rbEng.isChecked())
-        {
-            //Set language to English.
-            selectedLanguage = ENGLISH;
-        }
-        else if(rbSpan.isChecked())
-        {
-            //Set language to Spanish.
-            selectedLanguage = SPANISH;
-        }
-        else
-        {
-            //Default: leave language setting as-is on device.
-            selectedLanguage = Locale.getDefault().getDisplayLanguage();
-        }
-
-        changeLanguage(selectedLanguage);
-
-
-
     }
 
     public void showDatePickerDialog(View v)
@@ -170,6 +145,26 @@ public class GatherMoreInfo extends AppCompatActivity implements AdapterView.OnI
 
         if( v.getId() == b.getId() )
         {
+
+            //Determine chosen language.
+            if(rbSpan.isChecked())
+            {
+                language = "Spanish";
+            }
+            else
+            {
+                //Default to English.
+                language = "English";
+            }
+
+
+            Log.d("member_type",memberType);
+
+            //Save data to file first, then go to next Activity.
+            writeToFile(memberType+","+language+","+name.getText().toString()+","+address.getText().toString()+","+email.getText().toString()+","+time.getText().toString()+","+date.getText().toString()+",",this);
+
+            showInfo();
+
             intent = new Intent(this, PersonalActivity.class);
             startActivity(intent);
         }
@@ -179,50 +174,6 @@ public class GatherMoreInfo extends AppCompatActivity implements AdapterView.OnI
         }
 
     }
-
-
-    //Save to name, email, and address to file.  Time and date?  Append the data, not overwrite.
-    private void saveFile()
-    {
-        String path= "";
-        String filename = "";
-        File f;
-        BufferedWriter writer = null;
-
-        String sName, sAddress, sEmail;
-
-        sName       = name.getText().toString();
-        sAddress    = address.getText().toString();
-        sEmail      = email.getText().toString();
-
-        try
-        {
-            f = new File(path);
-            writer = new BufferedWriter(new FileWriter(f));
-
-
-            writer.write(sName);
-            writer.newLine();
-            writer.write(sAddress);
-            writer.newLine();
-            writer.write(sEmail);
-            writer.newLine();
-
-
-
-
-        }
-        catch(Exception e)
-        {
-
-        }
-        finally
-        {
-            try{if(writer != null)writer.close();}catch(Exception x){}
-        }
-    }
-
-
 
     //DATE PICKER INNER CLASS.
     public static class DatePickerFragment extends DialogFragment
@@ -286,4 +237,102 @@ public class GatherMoreInfo extends AppCompatActivity implements AdapterView.OnI
             time.setText(hourOfDay+":"+minute+ampm);
         }
     }
+
+
+    //************************************************
+    //*********** FILE I/O ***************************
+    //************************************************
+
+    private void writeToFile(String data,Context context)
+    {
+        OutputStreamWriter outputStreamWriter = null;
+
+        try
+        {
+            outputStreamWriter = new OutputStreamWriter(context.openFileOutput(FILE_NAME, Context.MODE_APPEND));
+            outputStreamWriter.write(data.replaceAll(",",System.lineSeparator()));
+
+            Log.d("write_status","Wrote successfully");
+        }
+        catch (IOException e)
+        {
+            Log.e("Exception", "File write failed: " + e.toString());
+            System.out.println("FIle Failed to open");
+        }
+        finally
+        {
+            try
+            {
+                if(outputStreamWriter != null)
+                {
+                    outputStreamWriter.close();
+                }
+            }
+            catch(IOException io)
+            {
+
+            }
+        }
+    }
+
+    private void openFile(String data,Context context) {
+
+        try
+        {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput(FILE_NAME, Context.MODE_PRIVATE));
+
+            Log.d("read_status","Opened successfully");
+        }
+        catch (IOException e)
+        {
+            Log.e("Exception", "File write failed: " + e.toString());
+            System.out.println("FIle Failed to open");
+        }
+    }
+    private String readFromFile(Context context)
+    {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = context.openFileInput(FILE_NAME);
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            Log.e("login activity", "File not found: " + e.toString());
+        }
+        catch (IOException e)
+        {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return ret;
+    }
+
+
+    private void showInfo()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Data Collected:"+System.lineSeparator());
+
+        sb.append(readFromFile(this));
+
+        Toast.makeText(this,sb.toString(),Toast.LENGTH_SHORT).show();
+    }
+
 }
